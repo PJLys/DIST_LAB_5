ce52b98204e51472203e2bec8488bd80f9dfe260
Ibe Van Thillo <Ibe.VanThillo@student.uantwerpen.be>
2023-04-24 12:26:23 +0200

    Extract naming service methods into separate class

---
+++ src/main/java/dist/group2/NamingServer/NamingClient.java
@@ -0,0 +1,62 @@
+package dist.group2.NamingServer;
+
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NamingClient {
+    private static final RestTemplate restTemplate = new RestTemplate();
+    private static String name = null;
+    private static String baseUrl = null;
+
+    public static void setName(String name) {
+        NamingClient.name = name;
+    }
+
+    public static void setBaseUrl(String baseUrl) {
+        NamingClient.baseUrl = baseUrl;
+    }
+
+    public static void addNode(String nodeName, String IPAddress) {
+        String url = baseUrl;
+
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("nodeName", nodeName);
+        requestBody.put("IPAddress", IPAddress);
+        try {
+            System.out.println("<" + name + "> - Add node with name " + nodeName + " and IP address " + IPAddress);
+            restTemplate.postForObject(url, requestBody, Void.class);
+        } catch (Exception e) {
+            throw new RuntimeException("A hash collision occurred for node " + nodeName);
+        }
+    }
+
+    public static void deleteNode(String nodeName) {
+        String url = baseUrl + "/" + nodeName;
+        restTemplate.delete(url);
+        System.out.println("<" + name + "> - Deleted node with name " + nodeName);
+    }
+
+    public static void findFile(String fileName) {
+        String url = baseUrl + "?fileName=" + fileName;
+        try {
+            String IPAddress = restTemplate.getForObject(url, String.class);
+            System.out.println("<" + name + "> - " + fileName + " is stored at IPAddress " + IPAddress);
+        } catch (Exception e) {
+            throw new RuntimeException("There are no nodes in the database");
+        }
+    }
+
+    public static String getIPAddress(int nodeID) {
+        String url = baseUrl + "/translate" + "?nodeID=" + nodeID;
+        try {
+            String IPAddress = restTemplate.getForObject(url, String.class);
+            System.out.println("<" + name + "> - Node with ID " + nodeID + " has IPAddress " + IPAddress);
+            return IPAddress;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to find IPAddress of node with ID " + nodeID);
+        }
+    }
+}
+
