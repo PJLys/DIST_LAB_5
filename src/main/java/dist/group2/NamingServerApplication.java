4794f862c1529693fe576d744094890af836a680
Stijn Van Raemdonck <van.raemdonck.stijn2@gmail.com>
2023-04-24 09:38:49 +0200

    Added Maven project

--- src/main/java/dist/group2/NamingServerApplication.java
+++ src/main/java/dist/group2/NamingServerApplication.java
@@ -3,8 +3,6 @@ package dist.group2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.net.UnknownHostException;
-
 @SpringBootApplication
 public class NamingServerApplication {
 	public static void main(String[] args) {
