a94b3dea599b5bab302681087f81e629615f29ce
Stijn Van Raemdonck <van.raemdonck.stijn2@gmail.com>
2023-05-25 20:45:35 +0200

    rest debug 52

--- src/main/java/dist/group2/ReplicationClient.java
+++ src/main/java/dist/group2/ReplicationClient.java
@@ -229,7 +229,25 @@ public class ReplicationClient implements Runnable{
             DiscoveryClient.failure();
         }
 
+        // Send a warning to the owners of these files so they can delete their replicated versions
+        assert localFiles != null;
+        for (File file : localFiles) {
+            // Get info of the file
+            String fileName = file.getName();
+            System.out.println("FILENAME " + fileName);
+            String filePath = local_file_path.toString() + '/' + fileName;
 
+            // The destination is the owner of the file instead of the previous node
+            // String destinationIP = NamingClient.findFile(fileName);
+
+            System.out.println("Send warning to delete file " + file.getName() + " to node " + previousNodeIP);
+
+            // Warn the owner of the file to delete the replicated file
+            sendFileToNode(filePath, null, previousNodeIP, "ENTRY_DELETE");
+            if (!Objects.equals(previousNodeIP, IPAddress)) {
+                sleep(300);
+            }
+        }
 
         // Send the replicated files and their logs to the previous node which will become the new owner of the file.
         // When the previous node already stores this file locally -> send it to its previous node
@@ -243,7 +261,9 @@ public class ReplicationClient implements Runnable{
 
             // Transfer the file and its log to the previous node
             sendFileToNode(filePath, logPath, previousNodeIP, "ENTRY_SHUTDOWN_REPLICATE");
-            sleep(300);
+            if (!Objects.equals(previousNodeIP, IPAddress)) {
+                sleep(300);
+            }
         }
     }
 
